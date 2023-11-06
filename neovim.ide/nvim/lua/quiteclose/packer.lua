e91bb262e6f0def84e36d1cf8e5e0b8bae25a115
QuiteClose <dean@quiteclose.net>
2023-11-06 04:50:47 +0000

    add copilot

--- neovim.ide/nvim/lua/quiteclose/packer.lua
+++ neovim.ide/nvim/lua/quiteclose/packer.lua
@@ -18,6 +18,7 @@ return require('packer').startup(function(use)
   use('theprimeagen/harpoon')
   use('mbbill/undotree')
   use('tpope/vim-fugitive')
+  use('github/copilot.vim')
 
   use {
     'VonHeikemen/lsp-zero.nvim',
@@ -42,4 +43,5 @@ return require('packer').startup(function(use)
       vim.cmd.colorscheme 'solarized'
     end
   }
+
 end)
