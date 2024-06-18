3aa22139507109ac19a81e9b74ff8e2b92eaaf56
deaclose <deaclose@cisco.com>
2024-06-17 17:20:49 -0700

    Ignore python warnings.

--- neovim.ide/nvim/after/plugin/lsp.lua
+++ neovim.ide/nvim/after/plugin/lsp.lua
@@ -33,12 +33,15 @@ require('lspconfig').pylsp.setup{
       plugins = {
         pycodestyle = {
           ignore = {
+            'E125', --continuation line with same indent as next logical line
             'E221', --multiple spaces before operator
             'E225', --missing whitespace around operator
             'E226', --missing whitespace around arithmetic operator
+            'E302', --expected 2 blank lines, found 1
+            'E305', --expected 2 blank lines after class or function definition
             'E501', --line too long
             'W391', --blank line at end of file
-          },  
+          },
           maxLineLength = 100
         }
       }
