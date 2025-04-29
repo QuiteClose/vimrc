d01f5063723d25004d9dd9206fb2e9eb7be4e1b7
QuiteClose <dean@quiteclose.net>
2025-04-29 04:44:22 +0000

    Add highlights

--- neovim.ide/nvim/lua/quiteclose/filetype.lua
+++ neovim.ide/nvim/lua/quiteclose/filetype.lua
@@ -1,10 +1,10 @@
-vim.api.nvim_create_augroup("setFiletypeGo", { clear = true })
+vim.api.nvim_create_augroup("setFiletypeTab", { clear = true })
 vim.api.nvim_create_autocmd("Filetype", {
-  group = "setFiletypeGo",
-  pattern = "go",
+  group = "setFiletypeTab",
+  pattern = "Dockerfile,go,Makefile,zig",
   callback = function()
     vim.schedule(function()
-      print("setFiletypeGo called")
+      print("setFiletypeTab called")
       vim.opt.shiftwidth = 4
       vim.opt.tabstop = 4
       vim.opt.expandtab = false
@@ -13,6 +13,44 @@ vim.api.nvim_create_autocmd("Filetype", {
         trail = "·",
         lead = "·",
         tab = "  ",
+      }
+    end)
+  end
+})
+vim.api.nvim_create_augroup("setFiletypeSpace", { clear = true })
+vim.api.nvim_create_autocmd("Filetype", {
+  group = "setFiletypeSpace",
+  pattern = "md,py,txt",
+  callback = function()
+    vim.schedule(function()
+      print("setFiletypeSpace called")
+      vim.opt.shiftwidth = 4
+      vim.opt.tabstop = 4
+      vim.opt.expandtab = true
+      vim.opt.listchars = {
+        lead = " ",
+        nbsp = "~",
+        tab = "··",
+        trail = "·",
+      }
+    end)
+  end
+})
+vim.api.nvim_create_augroup("setFiletypeWebdev", { clear = true })
+vim.api.nvim_create_autocmd("Filetype", {
+  group = "setFiletypeWebdev",
+  pattern = "html,css,scss,js,ts,jsx,tsx",
+  callback = function()
+    vim.schedule(function()
+      print("setFiletypeWebdev called")
+      vim.opt.shiftwidth = 2
+      vim.opt.tabstop = 2
+      vim.opt.expandtab = true
+      vim.opt.listchars = {
+        lead = " ",
+        nbsp = "~",
+        tab = "··",
+        trail = "·",
       }
     end)
   end
--- neovim.ide/nvim/lua/quiteclose/set.lua
+++ neovim.ide/nvim/lua/quiteclose/set.lua
@@ -4,11 +4,6 @@ vim.opt.nu = true
 vim.opt.relativenumber = true
 vim.opt.numberwidth=3
 
-vim.opt.tabstop = 4
-vim.opt.softtabstop = 4
-vim.opt.shiftwidth = 4
-vim.opt.expandtab = true
-
 vim.opt.smartindent = true
 
 vim.opt.wrap = false
@@ -30,11 +25,4 @@ vim.opt.isfname:append("@-@")
 vim.opt.updatetime = 50
 
 vim.opt.colorcolumn = "80"
-
-vim.opt.listchars = {
-  nbsp = "~",
-  tab = "»»",
-  trail = "·"
-}
-vim.opt.list = true
 
